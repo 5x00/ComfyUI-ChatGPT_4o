b62a7e0878835b0a2028b99565ca496e4714403c
subin <93209538+design-5k@users.noreply.github.com>
2024-11-29 14:07:58 +0530

    Update 29-11 2

--- __init__.py
+++ __init__.py
@@ -1,52 +1,7 @@
-from io import BytesIO
-import os
-import base64
-from openai import OpenAI
-from PIL import Image, ImageOps
-from torchvision.transforms import ToPILImage
+from .tagger import GPT4oNode
 
-class GPT4oNode:
-    @classmethod
-    def INPUT_TYPES(cls):
-        return {
-            "required": { "image_in" : ("IMAGE", {}) },
-            "required": { "prompt_in" : ("STRING", {}) },
-            "required": { "api" : ("STRING", {}) },
-        }
+NODE_CLASS_MAPPINGS = {
+    "GPT4o Tagger" : GPT4oNode,
+}
 
-    RETURN_TYPES = ("STRING")
-    RETURN_NAMES = ("text_out",)
-    CATEGORY = "5x00/GPT4o"
-    FUNCTION = "create_caption"
-
-    def create_caption(self, api, image_in, prompt_in):
-
-        # Initialize OpenAI client
-        client = OpenAI(api_key=api)
-
-        # Convert and resize image
-        transform = ToPILImage()
-        pil_image = transform(image_in.squeeze(0))
-        max_dimension = 512
-        pil_image.thumbnail((max_dimension, max_dimension))
-        buffer = BytesIO()
-        pil_image.save(buffer, format="JPEG")
-        buffer.seek(0) 
-        base64_image = base64.b64encode(buffer.getvalue()).decode("utf-8")
-        buffer.close()
-
-        # ChatGPT completions
-        response = client.chat.completions.create(
-            model="gpt-4o",
-            messages=[
-                {
-                    "role": "user",
-                    "content": [
-                        {"type": "text", "text": prompt_in},
-                        {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}}
-                    ],
-                }
-            ],
-        )
-        caption = response.choices[0].message.content 
-        return caption
+__all__ = ['NODE_CLASS_MAPPINGS']
